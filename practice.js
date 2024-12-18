34b3e359ea9f729785cfe590fca8d3bc9b2ed730
divyaramsaran <bagudivyaramsaran@gmail.com>
2024-12-18 09:56:34 +0530

    modified to cycle

--- practice.js
+++ practice.js
@@ -1,1 +1,23 @@
-console.log("ram");
+const fruits = ["goa", "banana", "pineapple"];
+const persons = ["ram", "saran", "divya", 'divya'];
+
+const fruitcollection = function (fruits) {
+  let index = -1;
+
+  return function (person) {
+    index++;
+
+    if(index >= 3) {
+      index = 0 ;
+    }
+
+    return person + " " + fruits[index];
+  }
+}
+
+const cycle = function () {
+ const func = fruitcollection(fruits);
+ return persons.map(func);
+}
+
+console.log(cycle());
